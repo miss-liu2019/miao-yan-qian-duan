2e6b6f626a878d8bbaaed7b363ee70953f924baf
Dich丶 <1591391677@qq.com>
2019-09-02 09:08:57 +0800

    uu

--- src/components/Pay/index.js
+++ src/components/Pay/index.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
 import pay from '../../asset/pay.jpg'
+import './pay.css'
+import {Icon} from 'antd-mobile'
 export default class index extends Component {
+    componentDidMount(){
+        // console.log(this.props.location.state)
+        if(this.props.location.state){
+            this.props.location.state('请扫码')
+        }
+    }
+    returnHandle = () => {
+        this.props.history.push('/movie/hot')
+    }
     render() {
         return (
-            <div>
+            <div className = 'pay_code'>
+                <div onClick = {this.returnHandle} className = 'return-box'>
+                    <Icon type='left' size='lg' />
+                    <span className='return-admin'>返回</span>
+                </div>
                 <img src = {pay} alt = '支付' title = '请扫码' />
             </div>
         )
